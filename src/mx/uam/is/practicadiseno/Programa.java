e1826bf47c3115849701c8c038218970c9bdf935
williampalacios <mich.palacios.hdz@gmail.com>
2021-05-16 19:04:59 -0500

    4.2 y 4.3 implementados

--- src/mx/uam/is/practicadiseno/Programa.java
+++ src/mx/uam/is/practicadiseno/Programa.java
@@ -1,8 +1,6 @@
 package mx.uam.is.practicadiseno;
 
-import mx.uam.is.practicadiseno.negocio.ManejadorImpl;
-import mx.uam.is.practicadiseno.datos.MapeadorDatosImpl;
-import mx.uam.is.practicadiseno.vista.Ventana;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Programa {
 
@@ -11,23 +9,9 @@ public class Programa {
 	 */
 	public static void main(String[] args) {
 
-		// Crea el mapedor de datos
-		MapeadorDatosImpl mapeador = new MapeadorDatosImpl();
+		// Carga el application context
+new ClassPathXmlApplicationContext("mx/uam/is/practicadiseno/applicationContext.xml");
 
-		// Crea el manejador
-
-		ManejadorImpl manejador = new ManejadorImpl(mapeador);
-		//ManejadorImpl manejador = new ManejadorImpl();
-
-		// Crea la ventana y le pasa referencia
-		Ventana ventana = new Ventana(manejador);
-		Ventana ventana2 = new Ventana(manejador);
-		Ventana ventana3 = new Ventana(manejador);
-
-		// Muestra la ventana arrancando asi el programa
-		ventana.setVisible(true);
-		ventana2.setVisible(true);
-		ventana3.setVisible(true);
 	}
-
 }
+
